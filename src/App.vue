0828cee41164299e9240fbfadf82d804b8ecbaff
Gijs van Dam <gvandam@gmail.com>
2018-03-21 18:00:50 +0800

    Really depend on the bootstrap npm package, change webpack.config.js, Add Arizonia font to App component.

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,12 @@
 <template>
     <div class="container">
-    
+        <h1>Hello!</h1>
+        <p>Tekst in raar font</p>
+        <div class="progress">
+            <div class="progress-bar" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%;">
+                70%
+            </div>
+        </div>
     </div>
 </template>
 
@@ -11,4 +17,10 @@
 </script>
 
 <style>
+    @import url("https://fonts.googleapis.com/css?family=Arizonia");
+
+    p {
+        font-family: Arizonia;
+        font-size: 12em;
+    }
 </style>
--- webpack.config.js
+++ webpack.config.js
@@ -22,6 +22,22 @@ module.exports = {
         loader: 'babel',
         exclude: /node_modules/
       },
+      { 
+        test: /\.css$/, 
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(woff|woff2)(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/font-woff'
+      },
+      {
+        test: /\.ttf(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'url?limit=10000&mimetype=application/octet-stream'
+      },
+      {
+        test: /\.eot(\?v=\d+\.\d+\.\d+)?$/, 
+        loader: 'file'
+      },
       {
         test: /\.(png|jpg|gif|svg)$/,
         loader: 'file',
